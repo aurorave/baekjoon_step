621fc73f348e1b87de841a193d264e7d38b61d55
aurorave <44628138+aurorave@users.noreply.github.com>
2023-01-16 15:40:26 +0900

    m<45일 때 m+= 값을 어떻게 잡느냐에 따라 사고 흐름과 코드가 단순해진다.
    else:(m>=45)에 대한 언급 자체가 필요 없어질 수 있다는 것

---
+++ 2. 조건문/2884.py
@@ -0,0 +1,15 @@
+h, m = input().split()  
+
+h = int(h)
+m = int(m)
+################ map(int, input().split())
+    
+if m < 45:
+    if h == 0:
+        h = 23
+        m += 60
+    else:
+        h -= 1
+        m += 60
+        
+print(h, m-45)
